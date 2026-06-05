b13c08e1cb0b5cfeef3b122290198a24f90693cb
Campos.Mat <matheuscampos.mo@gmail.com>
2026-06-05 13:44:17 -0300

    edição da área de pesquisa e consulta do livros.php, será atualizado e editado mais tarde pós afazeres!

--- livros.php
+++ livros.php
@@ -1,5 +1,30 @@
 <?php
+global $livro_1;
 include "dados.php";
+
+session_start();
+
+if(isset($_POST["pesquisa"])) {
+
+    $pesquisa = $_POST["pesquisa"];
+        if($pesquisa == "") {
+
+            echo "Preencha o campo de Pesquisa.";
+
+        }else{
+
+            if($pesquisa == "Codigo Limpo" || $pesquisa == "Outros jietos de usar a boca" || $pesquisa == "O que o sol faz com as flores" || $pesquisa == "Mitologia Nordica") {
+
+                    $SESSION["pesquisa"] = $pesquisa;
+
+            }else{
+
+                echo "Livro não encontrado ou Idisponivel";
+            }
+        }
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +40,29 @@ include "dados.php";
 <?php include "menu.php"; ?>
 
 <div class="container">
-    <h3>Livros</h3>
+    <?php foreach ($livros as $livro): ?>
+        <div class="card mb-3">
+            <div class="card-body">
+                <h3 class="card-title"><?= $livro["titulo"] ?></h3>
+                <p><strong>Autor: </strong> <?= $livro["autor"]?></p>
+                <p><strong>Categoria: </strong><?= $livro["categoria"]?></p>
+                <p><strong>Ano: </strong><?= $livro["ano"]?></p>
+                <p><strong>Quantidade: </strong><?= $livro["quantidade"]?></p>
+            </div>
+        </div>
+    <?php endforeach; ?>
+
+    <?php foreach ($livros as $livro_2): ?>
+        <div class="card mb-3">
+            <div class="card-body">
+                <h3 class="card-title"><?= $livro_2["titulo"] ?></h3>
+                <p><strong>Autor: </strong> <?= $livro["autor"]?></p>
+                <p><strong>Categoria: </strong><?= $livro["categoria"]?></p>
+                <p><strong>Ano: </strong><?= $livro_2["ano"]?></p>
+                <p><strong>Quantidade: </strong><?= $livro_2["quantidade"]?></p>
+    <?php endforeach; ?>
+            </div>
+        </div>
 </div>
 
 </body>
