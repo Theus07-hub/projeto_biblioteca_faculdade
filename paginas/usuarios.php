8b37716009d1d74ca7d7b6e8a2a17223557ec788
Campos.Mat <matheuscampos.mo@gmail.com>
2026-06-09 17:05:17 -0300

    Rascunho de alteracao. Arquivo (usuarios.php) Deixei um teste doque pode ser usado para puxar os usuários criado, sem precisar adicionar  manualmente.

--- paginas/usuarios.php
+++ paginas/usuarios.php
@@ -1,14 +1,5 @@
-<?php include "../config/menu.php"; ?>
-<?php include "../config/conexao.php";
-
-$sql= "SELECT * FROM usuario WHERE name";
-
-
-
-
-
-
-
+<?php include "menu.php"; ?>
+<?php include "conexao.php";
 ?>
 
 
@@ -20,7 +11,26 @@ $sql= "SELECT * FROM usuario WHERE name";
 
 </head>
 <body>
+<!--Area de perfil HTML-->
+<div class="row">
+    <div class="perfil">
+        <div class="perfil-body">
+            <!-- a sigla $stmt, serve para puxar informações do banco de dados -->
+            <?php $sql= "SELECT id, nome, email FROM usuario";
+            $stmt = $pdo = prepare($sql);
+            $stmt->execute(['id' => 1]);
+            $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
 
-<div id="username">  </div>
+            if ($usuario) {
+                echo "Nome: " .htmlspecialchars($usuario['nome']). "<br>";
+                echo "Email: " .htmlspecialchars($usuario['email']). "<br>";
+
+            }else{
+                echo "Perfl não encontrado";
+            }
+            ?>
+        </div>
+    </div>
+</div>
 
 </body>
